72907d27fdef0c34d5c518ec8b696847364df9ce
favianharya <t-favian.adrian@traveloka.com>
2025-09-17 00:01:48 +0700

    [RAG-EXPLORE][1.0.0] Add Rag Exploration Project

--- analysis/rag_explore/README.md
+++ analysis/rag_explore/README.md
@@ -10,69 +10,41 @@
 
 ```
 
-# 🦉 Tokopaedi Scraper
+# 🧠 RAG Pipeline with LangChain, HuggingFace, and FAISS
 
-Easily collect and analyze customer reviews from Tokopedia with a fast and reliable scraping tool.
+This project demonstrates how to build a **Retrieval-Augmented Generation (RAG) pipeline** using:
 
----
+- [LangChain](https://www.langchain.com/)
+- [HuggingFace Transformers](https://huggingface.co/)
+- [FAISS](https://github.com/facebookresearch/faiss) for vector storage
+- A simple **logging system** for monitoring
 
-## ✨ Features
+It supports loading datasets from both:
 
-- 📥 Scrape reviews automatically from Tokopedia product pages
-- 📊 Gather insights from thousands of customer feedback
-- ⚡ Export data quickly for reporting and research
-
----
-
-## 📦 Requirements
-
-- `Python 3.11+`
-- `bs4`
-- `pandas`
-- `PyYAML`
-- `rich`
-- `selenium`
+- **Hugging Face Hub datasets**
+- **Local `.txt` files**
 
 ---
 
-## ⚙️ Installation
+## 🚀 Features
 
-Install `uv`:
+- Load dataset from Hugging Face (`HuggingFaceDatasetLoader`)
+- Load local text files (`TextLoader`)
+- Split documents into smaller chunks for embeddings
+- Generate embeddings with `HuggingFaceEmbeddings`
+- Store and retrieve vectors using FAISS
+- Save and reload FAISS indexes for faster future queries
+- Basic logger to track pipeline events
 
-```bash
-curl -LsSf https://astral.sh/uv/install.sh | sh
-```
+---
 
-Sync dependencies:
+## 📦 Installation and Run App
+
+Make sure you have Python 3.9+ and create a virtual environment:
 
 ```bash
 uv sync
+source .venv/bin/activate
+
+uv run main.py
 ```
-
-> This installs all dependencies from `uv.lock`.
-
----
-
-## 🚀 Usage
-
-Run the application:
-
-```bash
-cd tokoscrapedia
-make install-tokoscrapedia
-tokoscrapedia
-```
-
----
-
-## 📝 License
-
-MIT License — free to use and modify.
-
----
-
-## 🤝 Contributing
-
-Feel free to open an issue or submit a pull request to contribute new features or improvements!
-
----
---
+++ analysis/rag_explore/main.py
@@ -0,0 +1,136 @@
+
+from langchain_community.document_loaders import HuggingFaceDatasetLoader, TextLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+
+import logging
+logger = logging.getLogger(__name__)
+
+def setup_logger():
+    """
+    setup the logger with a stream handler and formatter
+    """
+
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        console_handler = logging.StreamHandler()
+        log_format = "%(asctime)s | %(levelname)s: %(message)s"
+        console_handler.setFormatter(logging.Formatter(log_format))
+        logger.addHandler(console_handler)
+
+setup_logger()
+
+class DatasetPreparation:
+    def __init__(
+        self, 
+        dataset_name: str = None,       
+        page_content_column: str = None,
+        txt_path: str = None,          
+        chunk_size: int = 1000,
+        chunk_overlap: int = 150
+    ):
+        self.dataset_name = dataset_name
+        self.page_content_column = page_content_column
+        self.txt_path = txt_path
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+    def load_hf_dataset(self):
+        """
+        Load a dataset from the Hugging Face Hub using HuggingFaceDatasetLoader.
+        """
+        logger.info("📥 Load dataset from Hugging Face Hub")
+        loader = HuggingFaceDatasetLoader(self.dataset_name, self.page_content_column)
+        logger.info(f"✅ Dataset '{self.dataset_name}' loaded successfully")
+        return loader.load()
+
+    def load_txt_dataset(self):
+        """
+        Load documents from a local text file using TextLoader.
+        """
+        logger.info(f"📥 Load dataset from local text file: {self.txt_path}")
+        loader = TextLoader(self.txt_path, encoding="utf-8")
+        logger.info(f"✅ Text file '{self.txt_path}' loaded successfully")
+        return loader.load()
+
+    def split_texts(self, texts):
+        """
+        Split documents into smaller chunks using RecursiveCharacterTextSplitter.
+        """
+        logger.info("✂️  Splitting documents into smaller chunks")
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap
+        )
+        return text_splitter.split_documents(texts)
+
+    def prepare(self):
+        """
+        Prepare dataset (either Hugging Face dataset or local txt file).
+        """
+        if self.txt_path:
+            logger.info("📃 Using local text file for dataset preparation")
+            data = self.load_txt_dataset()
+        else:
+            logger.info("😀 Using Hugging Face dataset for dataset preparation")
+            data = self.load_hf_dataset()
+        
+        docs = self.split_texts(data)
+        logger.info(f"✅ Dataset preparation completed with {len(docs)} chunks")
+        return docs
+
+class VectorStore:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+    
+    def embed(self):
+        logger.info(f"🧠 Initializing embeddings with model: {self.model_name}")
+        model_kwargs = {'device':'cpu'}
+        encode_kwargs = {'normalize_embeddings': False}
+        return HuggingFaceEmbeddings(
+            model_name=self.model_name,    
+            model_kwargs=model_kwargs, 
+            encode_kwargs=encode_kwargs 
+        )
+    
+    def stored_to_vector(self, docs):
+        logger.info("⚡ Converting documents to vector store using FAISS")
+        embedding = self.embed()
+        return FAISS.from_documents(docs, embedding)
+
+    def save(
+        self, 
+        vector_db, 
+        vector_name: str
+    ):
+        logger.info(f"💾 Saving vector store locally as: vector_db/{vector_name}")
+        rag_path = f"vector_db/{vector_name}"
+        vector_db.save_local(rag_path)
+
+    def load(
+        self, 
+        vector_name: str 
+    ):  
+        logger.info(f"📂 Loading vector store from: vector_db/{vector_name}")
+        rag_path = f"vector_db/{vector_name}"
+        embedding = self.embed()
+        return FAISS.load_local(rag_path, embedding, allow_dangerous_deserialization=True)
+
+def main():
+
+    embeddings = VectorStore(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    data_preparation=DatasetPreparation(
+        dataset_name='databricks/databricks-dolly-15k', 
+        page_content_column='context'   
+    )
+    data = data_preparation.prepare()
+    vector_db=embeddings.stored_to_vector(data)
+    embeddings.save(vector_db, vector_name="databricks_dolly_15k")
+
+    question = "What is cheesemaking?"
+    searchDocs = vector_db.similarity_search(question)
+    print(searchDocs[0].page_content)
+
+if __name__ == "__main__":
+    main()
